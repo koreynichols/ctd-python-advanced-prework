5b2a7c4e7acc3efe30daaeac5e06f5178d40682b
Korey Nichols <koreynichols1@gmail.com>
2026-08-15 17:53:49 -0700

    changed the fetch_country api to get countries by region. added fetch_regions to get all the regions from the api. displayed the regions and ask user to enter a region

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import requests
 
-def fetch_country(country):
-    response = requests.get("https://countries.dev/alpha/" + country)
+def fetch_regions():
+    response = requests.get("https://countries.dev/regions")
+    data = response.json()
+    return data;
+
+def fetch_country(region):
+    response = requests.get("https://countries.dev/region/" + region)
     data = response.json()
     print(data)
 
 def main():
-    fetch_country("US")
+    regions = fetch_regions()
+    print("Country Explorer")
+    print("================")
+    print("\nAvailable regions:")
 
+    for region in regions:
+        print(region)
+
+    region = input("\nEnter a region: ")
+    print(f"\nCountries in {region}")
+    print("======================")
+    
 if __name__ == "__main__":
     main()
